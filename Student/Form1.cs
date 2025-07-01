efb92d7f8e8ebee673f93f5e98f6535fa62db796
Jayanth Upthala <65483279+JayanthUpthala@users.noreply.github.com>
2025-07-01 18:19:59 -0400

    Add files via upload

---
+++ Student/Form1.cs
@@ -0,0 +1,19 @@
+namespace Student
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Student s1 = new Student();
+            s1.Id = 1234;
+            s1.Test1 = 85;
+            s1.
+        }
+    }
+}
---
+++ Student/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentApp
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public int Test1 { get; set; }
+        public int Test2 { get; set; }
+        
+        public string ComputeGrade()
+        {
+            string grade = "";
+            double avg = 0.4*Test1 + 0.6*Test2;
+            if (avg > 90)
+                grade = "A";
+            else if (avg > 80)
+                grade = "A-";
+            else if (avg > 70)
+                grade = "B";
+            else
+                grade = "C";
+            return grade;
+        }
+
+    }
+}
