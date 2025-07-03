d75a5e5891b173a87fba9a9afbc8ea1d01747fb0
Jayanth Upthala <65483279+JayanthUpthala@users.noreply.github.com>
2025-07-03 08:12:59 -0400

    Add files via upload

---
+++ WinFormsApp/DataLayer/IDataAccess.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBStudentApp_1216098.DataLayer
+{
+    internal interface IDataAccess
+    {
+        object GetSingleAnswer(string sql);
+        DataTable GetManyRowsCols(string sql);
+        int InsertUpdateDelete(string sql);
+    }
+}
---
+++ WinFormsApp/DataLayer/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using DBStudentApp_1216098.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBStudentApp_1216098.DataLayer
+{
+    internal interface IRepository
+    {
+        List<Course> GetAllCourses();
+        List<Enrollment> GetEnrollment(string courseNum);
+    }
+}
