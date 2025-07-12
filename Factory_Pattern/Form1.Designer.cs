7e9a3d868a578bc01f4b79f6c2f4fd3dc5bc2d16
Jayanth Upthala <65483279+JayanthUpthala@users.noreply.github.com>
2025-07-11 21:46:48 -0400

    Add files via upload

---
+++ Factory_Pattern/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace FacadePattern_1216098
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnMortgageApproval = new Button();
+            SuspendLayout();
+            // 
+            // btnMortgageApproval
+            // 
+            btnMortgageApproval.Location = new Point(189, 47);
+            btnMortgageApproval.Name = "btnMortgageApproval";
+            btnMortgageApproval.Size = new Size(175, 50);
+            btnMortgageApproval.TabIndex = 0;
+            btnMortgageApproval.Text = "Mortgage Approval";
+            btnMortgageApproval.UseVisualStyleBackColor = true;
+            btnMortgageApproval.Click += btnMortgageApproval_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnMortgageApproval);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnMortgageApproval;
+    }
+}
---
+++ Factory_Pattern/Form1.cs
@@ -0,0 +1,30 @@
+namespace FacadePattern_1216098
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMortgageApproval_Click(object sender, EventArgs e)
+        {
+            string reason = "";
+            MortgageApplicant mapp = new MortgageApplicant
+            {
+                FirstName = "Bill",
+                LastName = "Baker",
+                BankAccountNum = 12345,
+                LoanAccountNum = 12345,
+                SSNum = "111-22-3333",
+                LoanAmountAsked = 150000
+            };
+            MortgageFacadeSingleton mf = MortgageFacadeSingleton.Instance;
+            bool approved = mf.MortgageApproval(mapp, ref reason);
+            if (approved == true)
+                MessageBox.Show("Your application has been approved..");
+            else
+                MessageBox.Show("Your application has been denied..\n" + reason);
+        }
+    }
+}
