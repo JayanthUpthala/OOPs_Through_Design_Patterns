6cdfc2611d8fe7bb1cd072033584545da9a4b0ac
Jayanth Upthala <65483279+JayanthUpthala@users.noreply.github.com>
2025-07-25 19:50:32 -0400

    Add files via upload

---
+++ Template_Pattern/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TemplatePattern
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTemplate_Click(object sender, EventArgs e)
+        {
+            TemplateStudent tst = new StudentProcessingViaFile(@"D:\UB\CPSC501 - OOPs\Assignment8\Data\StudentData.txt", @"D:\UB\CPSC501 - OOPs\Assignment8\Data\StudentDataOut.txt");
+            tst.ReadAndProcessStudents();
+            // process students via DB
+            TemplateStudent tst2 = new StudentProcessingViaDB();
+            tst2.ReadAndProcessStudents();
+            MessageBox.Show("done processing..");
+        }
+
+        private void btnTemplateAndStrategy_Click(object sender, EventArgs e)
+        {
+            //IGradeStrategy igst = new ComputeGrade4060();
+            IGradeStrategy igst = new ComputeGrade5050();
+            TemplateStudent tst = new StudentProcessingViaFileV2(@"D:\UB\CPSC501 - OOPs\Assignment8\Data\StudentData.txt", @"D:\UB\CPSC501 - OOPs\Assignment8\Data\StudentDataOut.txt", igst);
+            tst.ReadAndProcessStudents();
+            MessageBox.Show("done..");
+        }
+    }
+}
---
+++ Template_Pattern/TemplateStudent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplatePattern
+{
+    internal abstract class TemplateStudent
+    {
+        protected List<Student> _STList = new List<Student>();
+        public List<Student> STList
+        {
+            get { return _STList; }
+            set { _STList = value; }
+        }
+        public abstract void ReadStudents();
+        public abstract void AssignGrades();
+        public abstract void SortStudents();
+        public abstract void StoreStudents();
+        // algorithm steps
+        public void ReadAndProcessStudents()
+        {
+            ReadStudents();
+            AssignGrades();
+            SortStudents();
+            StoreStudents();
+        }
+    }
+}
