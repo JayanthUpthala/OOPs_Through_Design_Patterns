2700d08a00dca11ba022563e9bdcef69f175b477
Jayanth Upthala <65483279+JayanthUpthala@users.noreply.github.com>
2025-07-28 21:24:55 -0400

    Add files via upload

---
+++ State_Pattern/ICheckingActivity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatePattern_1216098
+{
+    internal interface ICheckingActivity
+    {
+        void WithDrawMoney(double amt);
+        void DepositMoney(double amt);
+        void AddInterest();    
+    }
+}
